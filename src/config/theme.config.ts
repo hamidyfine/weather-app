a8c3ae32b55df837581782a83904e431f8b7aadc
Hamid Yaftian <hamid.yaftian@gmail.com>
2023-10-20 13:22:20 +0330

    feat: add tailwindcss and mantine

---
+++ src/config/theme.config.ts
@@ -0,0 +1,6 @@
+import type { MantineThemeOverride } from '@mantine/core';
+
+export const theme: MantineThemeOverride = {
+    fontFamily: 'Inter Variable, sans-serif',
+    headings: { fontFamily: 'Inter Variable, sans-serif' },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
+import { theme } from './config';
 import App from './App';
+import '@mantine/core/styles.css';
+import '@fontsource-variable/inter';
 import './main.scss';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <MantineProvider
+            defaultColorScheme="dark"
+            theme={theme}
+        >
+            <App />
+        </MantineProvider>
     </StrictMode>,
 );
