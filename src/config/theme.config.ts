260220a4bad1f0fe17f7187158bbda09be6f1e3f
Hamid Yaftian <hamid.yaftian@gmail.com>
2023-10-21 20:55:20 +0330

    feat: add theme and conditions configs

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -1,6 +1,9 @@
 import type { MantineThemeOverride } from '@mantine/core';
 
+/**
+ * Overrides the default font family with Inter and Comfortaa
+ */
 export const theme: MantineThemeOverride = {
     fontFamily: 'Inter Variable, sans-serif',
-    headings: { fontFamily: 'Inter Variable, sans-serif' },
+    headings: { fontFamily: 'Comfortaa Variable, sans-serif' },
 };
